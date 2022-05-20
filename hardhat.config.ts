3dfafd624785326d0cd6a5e9336781ecdedf188b
Aditya Chakraborty <adityachakra16@gmail.com>
2022-05-19 19:56:52 -0500

    deploy scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,21 @@
 import * as dotenv from "dotenv";
 
+// eslint-disable-next-line node/no-extraneous-import
+import glob from "glob";
+import path from "path";
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
-
 dotenv.config();
 
+if (!process.env.SKIP_LOAD) {
+  glob.sync("./tasks/**/*.ts").forEach(function (file) {
+    require(path.resolve(file));
+  });
+}
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -29,6 +36,28 @@ const config: HardhatUserConfig = {
       url: process.env.ROPSTEN_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    dev: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+      gas: 12000000,
+      blockGasLimit: 0x1fffffffffffff,
+      allowUnlimitedContractSize: true,
+      timeout: 1800000,
+    },
+    mumbai: {
+      url: "https://speedy-nodes-nyc.moralis.io/27686f41b7c9afc73b87dfa2/polygon/mumbai",
+      chainId: 80001,
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+    },
+    polygon: {
+      url: "https://speedy-nodes-nyc.moralis.io/f84f46508f22a737cbbdb355/polygon/mainnet",
+      chainId: 137,
+      accounts: {
+        mnemonic: "secret",
+      },
     },
   },
   gasReporter: {
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,44 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+import fs from "fs";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const PublishingLogic = await ethers.getContractFactory("PublishingLogic");
+  const publishingLogic = await PublishingLogic.deploy();
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await publishingLogic.deployed();
 
-  await greeter.deployed();
+  const hubLibs = {
+    "contracts/libraries/PublishingLogic.sol:PublishingLogic":
+      publishingLogic.address,
+  };
 
-  console.log("Greeter deployed to:", greeter.address);
+  const CredsHub = await ethers.getContractFactory("CredsHub", {
+    libraries: hubLibs,
+  });
+  const credsHub = await CredsHub.deploy();
+
+  await credsHub.deployed();
+
+  const ClaimModule = await ethers.getContractFactory("ClaimModule");
+  const claimModule = await ClaimModule.deploy();
+
+  await claimModule.deployed();
+
+  // const ReviewModule = await ethers.getContractFactory("ReviewModule");
+  // const reviewModule = await ReviewModule.deploy();
+
+  // await reviewModule.deployed();
+
+  const addrs = {
+    credHub: credsHub.address,
+    claimModule: claimModule.address,
+    publishingLogic: publishingLogic.address,
+  };
+  const json = JSON.stringify(addrs, null, 2);
+  console.log(json);
+  fs.writeFileSync("addresses.json", json, "utf-8");
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
